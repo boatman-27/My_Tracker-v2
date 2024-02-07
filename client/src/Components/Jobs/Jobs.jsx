52940387a34f4db5451e0dd14eb05dcb9c2d5f1f
Adham <adham46>
2024-02-08 02:04:55 +0400

    SHould worl?

--- client/src/Components/Jobs/Jobs.jsx
+++ client/src/Components/Jobs/Jobs.jsx
@@ -70,7 +70,7 @@ const Jobs = () => {
     )
       .then((response) => {
         console.log(response);
-        response.json();
+        return response.json();
       })
       .then((data) => console.log(data))
       .catch((error) => console.error("Error:", error));
