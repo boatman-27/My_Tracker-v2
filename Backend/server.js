4ca0c7fe876c427130a21a199cf5a1786a29a5fa
Adham <adham46>
2024-02-07 20:39:13 +0400

    .env

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import pg from "pg";
-// import env from "dotenv";
-// env.config();
+import env from "dotenv";
+env.config();
 
 const app = express();
 const port = 7001;
@@ -17,11 +17,11 @@ app.use(express.json());
 // });
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "Projects",
-  password: "Ao260221_@",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
